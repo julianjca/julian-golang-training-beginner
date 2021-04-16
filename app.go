f18e0d48711bd7d1c13aa0d7f9478fec5d61db80
Julian Anderson <juliancanderson@gmail.com>
2021-04-16 16:58:42 +0700

    chore: refactor byte into struct

--- app.go
+++ app.go
@@ -1,24 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
+type HelloResponse struct {
+	Message string `json:"message"`
+}
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(http.StatusOK)
-	jsonData := []byte(`{"status":"healthy"}`)
-	w.Write(jsonData)
+	jsonData := &HealthResponse{Status: "healthy"}
+
+	e, err := json.Marshal(jsonData)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	w.Write(e)
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(http.StatusOK)
-	jsonData := []byte(`{"message":"hello world"}`)
-	w.Write(jsonData)
+	jsonData := &HelloResponse{Message: "hello world"}
+
+	e, err := json.Marshal(jsonData)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	w.Write(e)
 }
 
 func handleRequests() {
